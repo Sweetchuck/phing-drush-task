7d6112d05027f608a6cde26a26786ba7720cf7b3
Dávid Andor <david-andor@kozpontiagy.hu>
2015-03-14 15:33:17 +0100

    Add returnProperty and ouputProperty attributes.

--- DrushTask.php
+++ DrushTask.php
@@ -9,17 +9,27 @@ require_once "phing/Task.php";
 /**
  * Class DrushParam.
  */
-class DrushParam {
+class DrushParam
+{
 
-  private $value;
+    /**
+     * @var string
+     */
+    protected $value = NULL;
 
-  public function addText($str) {
-    $this->value = $str;
-  }
+    /**
+     * @param string $value
+     */
+    public function addText($value) {
+        $this->value = $value;
+    }
 
-  public function getValue() {
-    return $this->value;
-  }
+    /**
+     * @return string
+     */
+    public function getValue() {
+        return $this->value;
+    }
 
 }
 
@@ -28,347 +38,428 @@ class DrushParam {
  */
 class DrushOption {
 
-  private $name;
-  private $value;
+    /**
+     * @var string
+     */
+    protected $name = '';
 
-  public function setName($str) {
-    $this->name = $str;
-  }
+    /**
+     * @var string
+     */
+    protected $value = '';
 
-  public function getName() {
-    return $this->name;
-  }
-
-  public function addText($str) {
-    $this->value = $str;
-  }
-
-  public function getValue() {
-    return $this->value;
-  }
-
-  public function __toString() {
-    $name  = $this->getName();
-    $value = $this->getValue();
-    $str = '--'.$name;
-    if (!empty($value)) {
-      $str .= '='.$value;
+    /**
+     * @param string $value
+     */
+    public function setName($value)
+    {
+        $this->name = $value;
     }
-    return $str;
-  }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function addText($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        $name = $this->getName();
+        $value = $this->getValue();
+        $return = '--' . $name;
+        if (!empty($value)) {
+            $return .= '=' . $value;
+        }
+
+        return $return;
+    }
 
 }
 
 /**
  * Class DrushTask.
  */
-class DrushTask extends Task {
+class DrushTask extends Task
+{
 
-  /**
-   * The message passed in the build file.
-   */
-  private $command = array();
+    /**
+     * @var string
+     */
+    protected $outputProperty = '';
 
-  /**
-   * @var string
-   */
-  private $bin = '';
+    /**
+     * The message passed in the build file.
+     */
+    protected $command = array();
 
-  /**
-   * @var string
-   */
-  private $uri = '';
+    /**
+     * @var string
+     */
+    protected $bin = '';
 
-  /**
-   * @var string
-   */
-  private $root = '';
+    /**
+     * @var string
+     */
+    protected $uri = '';
 
-  /**
-   * @var bool|null
-   */
-  private $assume = NULL;
+    /**
+     * @var string
+     */
+    protected $root = '';
 
-  /**
-   * @var bool
-   */
-  private $simulate = FALSE;
+    /**
+     * @var bool|null
+     */
+    protected $assume = NULL;
 
-  /**
-   * @var bool
-   */
-  private $pipe = FALSE;
+    /**
+     * @var boolean
+     */
+    protected $simulate = FALSE;
 
-  /**
-   * @var DrushOption[]
-   */
-  private $options = array();
+    /**
+     * @var boolean
+     */
+    protected $pipe = FALSE;
 
-  /**
-   * @var DrushParam[]
-   */
-  private $params = array();
+    /**
+     * @var DrushOption[]
+     */
+    protected $options = array();
 
-  /**
-   * @var string
-   */
-  private $returnGlue = "\n";
+    /**
+     * @var DrushParam[]
+     */
+    protected $params = array();
 
-  /**
-   * @var string
-   */
-  private $returnProperty = NULL;
+    /**
+     * @var string
+     */
+    protected $outputGlue = "\n";
 
-  /**
-   * @var bool
-   */
-  private $verbose = FALSE;
+    /**
+     * @var string
+     */
+    protected $returnProperty = NULL;
 
-  /**
-   * @var bool
-   */
-  private $haltOnError = TRUE;
+    /**
+     * @var boolean
+     */
+    protected $verbose = FALSE;
 
-  /**
-   * @var string
-   */
-  protected $cwd = '';
+    /**
+     * @var boolean
+     */
+    protected $haltOnError = TRUE;
 
-  /**
-   * The Drush command to run.
-   */
-  public function setCommand($str) {
-    $this->command = $str;
-  }
+    /**
+     * @var string
+     */
+    protected $dir = '';
 
-  /**
-   * Path the Drush executable.
-   */
-  public function setBin($str) {
-    $this->bin = $str;
-  }
-
-  /**
-   * Drupal root directory to use.
-   */
-  public function setRoot($str) {
-    $this->root = $str;
-  }
-
-  /**
-   * URI of the Drupal to use.
-   */
-  public function setUri($str) {
-    $this->uri = $str;
-  }
-
-  /**
-   * Assume 'yes' or 'no' to all prompts.
-   */
-  public function setAssume($var) {
-    if (is_string($var)) {
-      $this->assume = ($var === 'yes');
-    } else {
-      $this->assume = !!$var;
-    }
-  }
-
-  /**
-   * Simulate all relevant actions.
-   */
-  public function setSimulate($var) {
-    if (is_string($var)) {
-      $var = strtolower($var);
-      $this->simulate = ($var === 'yes' || $var === 'true');
-    } else {
-      $this->simulate = !!$var;
-    }
-  }
-
-  /**
-   * Use the pipe option.
-   */
-  public function setPipe($var) {
-    if (is_string($var)) {
-      $var = strtolower($var);
-      $this->pipe = ($var === 'yes' || $var === 'true');
-    } else {
-      $this->pipe = !!$var;
-    }
-  }
-
-  /**
-   * The 'glue' characters used between each line of the returned output.
-   */
-  public function setReturnGlue($str) {
-    $this->returnGlue = (string) $str;
-  }
-
-  /**
-   * The name of a Phing property to assign the Drush command's output to.
-   */
-  public function setReturnProperty($str) {
-    $this->returnProperty = $str;
-  }
-
-  /**
-   * @param string $var
-   */
-  public function setHaltOnError($var) {
-    if (is_string($var)) {
-      $var = strtolower($var);
-      $this->haltOnError = ($var === 'yes' || $var === 'true');
-    } else {
-      $this->haltOnError = !!$var;
-    }
-  }
-
-  /**
-   * @param string $value
-   *
-   * @return $this
-   */
-  public function setDir($value) {
-    $this->cwd = $value;
-
-    return $this;
-  }
-
-  /**
-   * Parameters for the Drush command.
-   */
-  public function createParam() {
-    $o = new DrushParam();
-    $this->params[] = $o;
-    return $o;
-  }
-
-  /**
-   * Options for the Drush command.
-   */
-  public function createOption() {
-    $o = new DrushOption();
-    $this->options[] = $o;
-    return $o;
-  }
-
-  /**
-   * Display extra information about the command.
-   */
-  public function setVerbose($var) {
-    if (is_string($var)) {
-      $this->verbose = ($var === 'yes');
-    } else {
-      $this->verbose = !!$var;
-    }
-  }
-
-  /**
-   * Initialize the task.
-   */
-  public function init() {
-    // Get default root, uri and binary from project.
-    $this->root = $this->getProject()->getProperty('drush.root');
-    $this->uri = $this->getProject()->getProperty('drush.uri');
-    $this->bin = $this->getProject()->getProperty('drush.bin');
-  }
-
-  /**
-   * The main entry point method.
-   */
-  public function main() {
-    $command = array();
-
-    $command[] = !empty($this->bin) ? $this->bin : 'drush';
-
-    $option = new DrushOption();
-    $option->setName('nocolor');
-    $this->options[] = $option;
-
-    if (!empty($this->root)) {
-      $option = new DrushOption();
-      $option->setName('root');
-      $option->addText($this->root);
-      $this->options[] = $option;
+    /**
+     * The Drush command to run.
+     *
+     * @param string $value
+     */
+    public function setCommand($value)
+    {
+        $this->command = $value;
     }
 
-    if (!empty($this->uri)) {
-      $option = new DrushOption();
-      $option->setName('uri');
-      $option->addText($this->uri);
-      $this->options[] = $option;
+    /**
+     * Path the Drush executable.
+     *
+     * @param string $value
+     */
+    public function setBin($value)
+    {
+        $this->bin = $value;
     }
 
-    if (is_bool($this->assume)) {
-      $option = new DrushOption();
-      $option->setName(($this->assume ? 'yes' : 'no'));
-      $this->options[] = $option;
+    /**
+     * URI of the Drupal to use.
+     *
+     * @param string $value
+     */
+    public function setUri($value)
+    {
+        $this->uri = $value;
     }
 
-    if ($this->simulate) {
-      $option = new DrushOption();
-      $option->setName('simulate');
-      $this->options[] = $option;
+    /**
+     * Assume 'yes' or 'no' to all prompts.
+     *
+     * @param boolean $value
+     */
+    public function setAssume($value)
+    {
+        if (is_string($value)) {
+            $this->assume = ($value === 'yes');
+        }
+        else {
+            $this->assume = !!$value;
+        }
     }
 
-    if ($this->pipe) {
-      $option = new DrushOption();
-      $option->setName('pipe');
-      $this->options[] = $option;
+    /**
+     * Simulate all relevant actions.
+     *
+     * @param boolean $value
+     */
+    public function setSimulate($value)
+    {
+        if (is_string($value)) {
+            $value = strtolower($value);
+            $this->simulate = ($value === 'yes' || $value === 'true');
+        }
+        else {
+            $this->simulate = (bool) $value;
+        }
     }
 
-    if ($this->verbose) {
-      $option = new DrushOption();
-      $option->setName('verbose');
-      $this->options[] = $option;
+    /**
+     * Use the pipe option.
+     *
+     * @param boolean $value
+     */
+    public function setPipe($value)
+    {
+        if (is_string($value)) {
+            $value = strtolower($value);
+            $this->pipe = ($value === 'yes' || $value === 'true');
+        }
+        else {
+            $this->pipe = (bool) $value;
+        }
     }
 
-    foreach ($this->options as $option) {
-      $command[] = (string) $option;
+    /**
+     * The 'glue' characters used between each line of the returned output.
+     *
+     * @param string $value
+     */
+    public function setOutputGlue($value)
+    {
+        $this->outputGlue = (string) $value;
     }
 
-    $command[] = $this->command;
-
-    foreach ($this->params as $param) {
-      $command[] = $param->getValue();
+    /**
+     * The name of a Phing property to assign the Drush command's output to.
+     */
+    public function setReturnProperty($str)
+    {
+        $this->returnProperty = $str;
     }
 
-    $command = implode(' ', $command);
-
-    // Execute Drush.
-    $this->log("Executing '$command'...");
-    $error_code = 0;
-    $output = array();
-
-    $original_directory = NULL;
-    if ($this->cwd) {
-      $original_directory = getcwd();
-      chdir($this->cwd);
+    /**
+     * @param string $value
+     */
+    public function setOutputProperty($value)
+    {
+        $this->outputProperty = $value;
     }
 
-    exec($command, $output, $error_code);
-
-    if ($original_directory) {
-      chdir($original_directory);
+    /**
+     * @param boolean $value
+     */
+    public function setHaltOnError($value)
+    {
+        if (is_string($value)) {
+            $value = strtolower($value);
+            $this->haltOnError = ($value === 'yes' || $value === 'true');
+        }
+        else {
+            $this->haltOnError = (bool) $value;
+        }
     }
 
-    // Collect Drush output for display through Phing's log.
-    foreach ($output as $line) {
-      $this->log($line);
+    /**
+     * @param string $value
+     */
+    public function setRoot($value)
+    {
+        $this->root = $value;
     }
 
-    // Set value of the 'pipe' property.
-    if (!empty($this->returnProperty)) {
-      $this->getProject()->setProperty($this->returnProperty, implode($this->returnGlue, $output));
+    /**
+     * @param string $value
+     */
+    public function setDir($value)
+    {
+        $this->dir = $value;
     }
 
-    // Build fail.
-    if ($this->haltOnError && $error_code != 0) {
-      throw new BuildException("Drush exited with code $error_code");
+    /**
+     * Display extra information about the command.
+     *
+     * @param boolean $value
+     */
+    public function setVerbose($value)
+    {
+        if (is_string($value)) {
+            $this->verbose = ($value === 'yes');
+        }
+        else {
+            $this->verbose = (bool) $value;
+        }
     }
 
-    return $error_code != 0;
-  }
+    /**
+     * Parameters for the Drush command.
+     */
+    public function createParam()
+    {
+        $o = new DrushParam();
+        $this->params[] = $o;
+
+        return $o;
+    }
+
+    /**
+     * Options for the Drush command.
+     */
+    public function createOption() {
+        $o = new DrushOption();
+        $this->options[] = $o;
+
+        return $o;
+    }
+
+    /**
+     * Initialize the task.
+     */
+    public function init()
+    {
+        $this->root = $this->getProject()->getProperty('drush.root');
+        $this->dir = $this->getProject()->getProperty('drush.dir');
+        $this->uri = $this->getProject()->getProperty('drush.uri');
+        $this->bin = $this->getProject()->getProperty('drush.bin');
+    }
+
+    /**
+     * The main entry point method.
+     */
+    public function main() {
+        $command = array();
+
+        $command[] = !empty($this->bin) ? $this->bin : 'drush';
+
+        $option = new DrushOption();
+        $option->setName('nocolor');
+        $this->options[] = $option;
+
+        if (!empty($this->root)) {
+            $option = new DrushOption();
+            $option->setName('root');
+            $option->addText($this->root);
+            $this->options[] = $option;
+        }
+
+        if (!empty($this->uri)) {
+            $option = new DrushOption();
+            $option->setName('uri');
+            $option->addText($this->uri);
+            $this->options[] = $option;
+        }
+
+        if (is_bool($this->assume)) {
+            $option = new DrushOption();
+            $option->setName(($this->assume ? 'yes' : 'no'));
+            $this->options[] = $option;
+        }
+
+        if ($this->simulate) {
+            $option = new DrushOption();
+            $option->setName('simulate');
+            $this->options[] = $option;
+        }
+
+        if ($this->pipe) {
+            $option = new DrushOption();
+            $option->setName('pipe');
+            $this->options[] = $option;
+        }
+
+        if ($this->verbose) {
+            $option = new DrushOption();
+            $option->setName('verbose');
+            $this->options[] = $option;
+        }
+
+        foreach ($this->options as $option) {
+            $command[] = (string) $option;
+        }
+
+        $command[] = $this->command;
+
+        foreach ($this->params as $param) {
+            $command[] = $param->getValue();
+        }
+
+        $command = implode(' ', $command);
+
+        // Execute Drush.
+        $this->log("Executing '$command'...");
+        $error_code = 0;
+        $output = array();
+
+        if ($this->dir && !chdir($this->dir)) {
+            throw new BuildException('Working directory is not exists.');
+        }
+
+        exec($command, $output, $error_code);
+
+        if ($this->dir) {
+            chdir($this->project->getBasedir());
+        }
+
+        // Collect Drush output for display through Phing's log.
+        foreach ($output as $line) {
+            $this->log($line);
+        }
+
+        // Set value of the 'pipe' property.
+        if (!empty($this->returnProperty)) {
+            $this
+                ->getProject()
+                ->setProperty($this->returnProperty, $error_code);
+        }
+
+        if (!empty($this->outputProperty)) {
+            $this
+                ->getProject()
+                ->setProperty($this->outputProperty, implode($this->outputGlue, $output));
+        }
+
+        // Build fail.
+        if ($this->haltOnError && $error_code != 0) {
+            throw new BuildException("Drush exited with code $error_code");
+        }
+
+        return $error_code != 0;
+    }
 
 }
 
