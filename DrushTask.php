554da38791cc2ffa25f7a1bd432731735431c9ef
Dávid Andor <david-andor@kozpontiagy.hu>
2015-09-25 10:34:35 +0200

    Resolv the path of the drush executable.

--- DrushTask.php
+++ DrushTask.php
@@ -370,7 +370,7 @@ class DrushTask extends Task
         $this->root = $this->getProject()->getProperty('drush.root');
         $this->dir = $this->getProject()->getProperty('drush.dir');
         $this->uri = $this->getProject()->getProperty('drush.uri');
-        $this->bin = $this->getProject()->getProperty('drush.bin');
+        $this->bin = realpath($this->getProject()->getProperty('drush.bin'));
     }
 
     /**
